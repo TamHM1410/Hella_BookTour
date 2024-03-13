3cfe8374d8a7e2d9f3b3978a04af3c88a901b688
TamHM1410 <hunhminhtam@gmail.com>
2024-03-13 11:31:48 +0700

    ditme ts

--- src/services/paymentService/payment.service.ts
+++ src/services/paymentService/payment.service.ts
@@ -119,40 +119,16 @@ class PaymentService {
       await this.prisma.$disconnect;
     }
   };
-  createNewPayment = async (currentData: {
-    paymentDate: string;
-    amount: number;
-    status: boolean;
-    refunded: boolean;
-    refundedTime: string;
-    refundedAmount: string;
-    paymentMethodId: number;
-    bookingId: number;
-    paymentName: string;
-  }) => {
+  createNewPayment = async (currentData:any) => {
     try {
       await this.prisma.$connect;
-      const data = await this.prisma.payment.create({
-        data: {
-          paymentDate: new Date(),
-          amount: currentData.amount,
-          status: true,
-          refunded: currentData.refunded,
-          refundedTime: currentData.refundedTime,
-          refundedAmount: currentData.refundedAmount,
-       
-          bookingId: currentData.bookingId,
-          paymentName: currentData.paymentName,
-        },
-      });
+   
 
-      if (data) {
-        return {
-          status: "Success",
-          statusCode: 201,
-          data: data,
-        };
-      }
+      return {
+        status: "Success",
+        statusCode: 201,
+      
+      };
     } catch (error) {
       return {
         status: "Internal Server Error",
